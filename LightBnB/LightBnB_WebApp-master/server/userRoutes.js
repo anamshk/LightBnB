372705666906c6bb3b722028a086cebccd723bb4
Anam Shaikh <anam.shk@outlook.com>
2021-11-25 20:06:55 -0500

    SQL queries in .js file

--- LightBnB/LightBnB_WebApp-master/server/userRoutes.js
+++ LightBnB/LightBnB_WebApp-master/server/userRoutes.js
@@ -7,15 +7,15 @@ module.exports = function(router, database) {
     const user = req.body;
     user.password = bcrypt.hashSync(user.password, 12);
     database.addUser(user)
-    .then(user => {
-      if (!user) {
-        res.send({error: "error"});
-        return;
-      }
-      req.session.userId = user.id;
-      res.send("🤗");
-    })
-    .catch(e => res.send(e));
+      .then(user => {
+        if (!user) {
+          res.send({error: "error"});
+          return;
+        }
+        req.session.userId = user.id;
+        res.send("🤗");
+      })
+      .catch(e => res.send(e));
   });
 
   /**
